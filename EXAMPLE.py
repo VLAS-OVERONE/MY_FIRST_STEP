da26dcf051bcafc378785a2e138b8c2d099abd95
VLAS <AndreyVlasenko1986@gmail.com>
2021-05-20 01:11:59 +0300

    SECOND COMMIT

---
+++ EXAMPLE.py
@@ -0,0 +1,20 @@
+# Пользователя просят ввести его возраст. Проверка на ошибки try - except - raise:
+
+
+def age(data):
+    if data.isnumeric():
+        data = int(data)
+        if 0<= data <= 150:
+            return data
+        else:
+            raise ValueError
+    else:
+        raise TypeError
+
+try:
+    s = age(input("Введите свой возраст:"))
+except ValueError:
+    print("Введите корректное значение возраста")
+except TypeError:
+    print("Введите числовое значение возраста")
+print("Программа завершена!")
